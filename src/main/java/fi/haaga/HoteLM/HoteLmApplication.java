f578b23bb63ccdabc4fec27e8805405dd52b969e
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-06-06 15:05:20 +0300

    First Push

---
+++ src/main/java/fi/haaga/HoteLM/HoteLmApplication.java
@@ -0,0 +1,13 @@
+package fi.haaga.HoteLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteLmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteLmApplication.class, args);
+	}
+
+}
