a8c38e5461a2e98fe752fedc1b49a66c69b163b3
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-06-08 22:51:22 +0300

    update 3

--- src/main/java/fi/haaga/HoteLM/entity/Booking.java
+++ src/main/java/fi/haaga/HoteLM/entity/Booking.java
@@ -2,9 +2,31 @@ package fi.haaga.HoteLM.entity;
 
 public class Booking {
     private Long id;
+    private String customerName;
+    private String roomType;
+    private String checkInDate;
+    private String checkOutDate;
+    private String status;
+    private String specialRequests;
+    private String paymentMethod;
+    private String bookingDate;
+
+    public Booking() {
+    }
+    public Booking(Long id, String customerName, String roomType, String checkInDate, String checkOutDate, String status, String specialRequests, String paymentMethod, String bookingDate) {
+        this.id = id;
+        this.customerName = customerName;
+        this.roomType = roomType;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.status = status;
+        this.specialRequests = specialRequests;
+        this.paymentMethod = paymentMethod;
+        this.bookingDate = bookingDate;
+    }
 
 
 }
 
 
-orehrweurs
+orehrw
